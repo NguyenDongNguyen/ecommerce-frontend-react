1115a9cbaa18ad2095959176ef5d0b1ead10c752
NguyenDongNguyen <ndn030802@gmail.com>
2024-03-01 13:56:38 +0700

    xu ly token bi het han o phia front end

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,11 @@ import ProductDetailPage from "./pages/ProductDetailPage/ProductDetailPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import { jwtDecode } from "jwt-decode";
+import { isJsonString } from "./ultis";
+import * as UserService from "./services/UserService";
+import { useDispatch, useSelector } from "react-redux";
+import { updateUser } from "./redux/slicers/userSlice";
 
 const Layout = () => {
     return (
@@ -23,18 +28,60 @@ const Layout = () => {
 };
 
 const App = () => {
-    // useEffect(() => {
-    //     fetchApi()
-    // }, [])
-    // const fetchApi = async () => {
-    //     const res = await axios.get(
-    //         `${process.env.REACT_APP_API_URL}/product/get-all`
-    //     );
-    //     return res.data
-    // }
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user);
 
-    // const query = useQuery({ queryKey: ["todos"], queryFn: fetchApi });
-    // console.log("query: ", query)
+    // lấy lại token trong local storage khi reload trang
+    useEffect(() => {
+        const { storageData, decoded } = handleDecoded();
+        console.log("🚀 ~ useEffect ~ decoded:", decoded);
+        if (decoded?.id) {
+            handleGetDetailsUser(decoded?.id, storageData);
+        }
+    }, []);
+
+    const handleDecoded = () => {
+        let storageData = user?.access_token || localStorage.getItem("access_token");
+        let decoded = {};
+        if (storageData && isJsonString(storageData) && !user?.access_token) {
+            storageData = JSON.parse(storageData);
+            decoded = jwtDecode(storageData);
+        }
+        return { decoded, storageData };
+    };
+
+    // refresh token bị hết hạn ở trong local storage
+    UserService.axiosJWT.interceptors.request.use(
+        async (config) => {
+            // Do something before request is sent
+            const currentTime = new Date();
+            const { decoded } = handleDecoded();
+            // let storageRefreshToken = localStorage.getItem("refresh_token");
+            // const refreshToken = JSON.parse(storageRefreshToken);
+            // const decodedRefreshToken = jwt_decode(refreshToken);
+            if (decoded?.exp < currentTime.getTime() / 1000) {
+                const data = await UserService.refreshToken();
+                config.headers["token"] = `Bearer ${data?.access_token}`;
+            }
+            return config;
+        },
+        (err) => {
+            return Promise.reject(err);
+        }
+    );
+
+    const handleGetDetailsUser = async (id, token) => {
+        // let storageRefreshToken = localStorage.getItem("refresh_token");
+        // const refreshToken = JSON.parse(storageRefreshToken);
+        const res = await UserService.getDetailsUser(id, token);
+        dispatch(
+            updateUser({
+                ...res?.data,
+                access_token: token,
+                // refreshToken: refreshToken,
+            })
+        );
+    };
 
     const router = createBrowserRouter([
         {
